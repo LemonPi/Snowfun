fe50a454733addba07846d6a49e2bcdd9bac0ff8
Johnson Zhong <johnsonzhong@hotmail.ca>
2017-03-12 19:04:23 +0100

    Fix $.show not always showing
    When setting passed or not, don't hide dialogs since that's being taken
    of by advanceText.

--- fun.js
+++ fun.js
@@ -1,271 +1,293 @@
-"use strict";
+'use strict';
 
 /* Third party code */
 
 /* http://upshots.org/javascript/jquery-hittestobject */
 
-$.fn.hitTestObject = function(selector){
-        var compares = $(selector);
-        var l = this.size();
-        var m = compares.size();
-        for(var i = 0; i < l; i++){
-           var bounds = this.get(i).getBoundingClientRect();
-            for(var j = 0; j < m; j++){
-               var compare = compares.get(j).getBoundingClientRect();
-               if(!(bounds.right < compare.left ||
-                    bounds.left > compare.right ||
-                    bounds.bottom < compare.top ||
-                    bounds.top > compare.bottom)){
-						return true;   
-                }
+$.fn.hitTestObject = function(selector) {
+    var compares = $(selector);
+    var l        = this.size();
+    var m        = compares.size();
+    for (var i = 0; i < l; i++) {
+        var bounds = this.get(i).getBoundingClientRect();
+        for (var j = 0; j < m; j++) {
+            var compare = compares.get(j).getBoundingClientRect();
+            if (!(bounds.right < compare.left || bounds.left > compare.right ||
+                  bounds.bottom < compare.top || bounds.top > compare.bottom)) {
+                return true;
             }
         }
-		return false;
-	};
+    }
+    return false;
+};
 
 /* end third-party code */
 
 var blocks = {
-	"var": {
-		create: function(state) {
-			var a = $("<div><a class='block var'/><a class='block-nodule blockafter varafter'/></div>").draggable({
-				drag: function(ev, ui) {
-					drawNoduleLine($(this).find(".block-nodule"));
-				}
-			});
-			return a;
-		},
-		tostate: function(div) {
-			return {val: div.text()};
-		}
-	},
-	"combine": {
-		create: function(state) {
-			var a = $('<div><a class="block combine">Combine</a><a class="block-nodule blockafter combineafter"/></div>').draggable();
-			return a;
-		}
-	},
-	"subroutine": {
-		create: function(state) {
-			var a = $('<div class="block subroutine"><div class="block-innodule"/><div class="block-nodule"/></div>').draggable();
-			return a;
-		}
-	},
-	"outvar": {
-		create: function(state) {
-			var a = $("<div><a class='block outvar'/></div>").draggable();
-			a.find(".block-text").text(state.val);
-			return a;
-		},
-	},
-	"if": {
-		create: function(state) {
-			var a = $("<div><a class='block if'/><a class='block-nodule blockafter varafter'/></div>").draggable({
-				drag: function(ev, ui) {
-					drawNoduleLine($(this).find(".block-nodule"));
-				}
-			});
-			return a;
-		}
-	},
-	"for": {
-		create: function(state) {
-			var a = $("<div><a class='block for'/><a class='block-nodule blockafter varafter'/></div>").draggable({
-				drag: function(ev, ui) {
-					drawNoduleLine($(this).find(".block-nodule"));
-				}
-			});
-			return a;
-		}
-	},
+    'var': {
+        create: function(state) {
+            var a =
+                $('<div><a class=\'block var\'/><a class=\'block-nodule blockafter varafter\'/></div>')
+                    .draggable({
+                        drag: function(ev, ui) {
+                            drawNoduleLine($(this).find('.block-nodule'));
+                        }
+                    });
+            return a;
+        },
+        tostate: function(div) {
+            return {val: div.text()};
+        }
+    },
+    'combine': {
+        create: function(state) {
+            var a =
+                $('<div><a class="block combine">Combine</a><a class="block-nodule blockafter combineafter"/></div>')
+                    .draggable();
+            return a;
+        }
+    },
+    'subroutine': {
+        create: function(state) {
+            var a =
+                $('<div class="block subroutine"><div class="block-innodule"/><div class="block-nodule"/></div>')
+                    .draggable();
+            return a;
+        }
+    },
+    'outvar': {
+        create: function(state) {
+            var a = $('<div><a class=\'block outvar\'/></div>').draggable();
+            a.find('.block-text').text(state.val);
+            return a;
+        },
+    },
+    'if': {
+        create: function(state) {
+            var a =
+                $('<div><a class=\'block if\'/><a class=\'block-nodule blockafter varafter\'/></div>')
+                    .draggable({
+                        drag: function(ev, ui) {
+                            drawNoduleLine($(this).find('.block-nodule'));
+                        }
+                    });
+            return a;
+        }
+    },
+    'for': {
+        create: function(state) {
+            var a =
+                $('<div><a class=\'block for\'/><a class=\'block-nodule blockafter varafter\'/></div>')
+                    .draggable({
+                        drag: function(ev, ui) {
+                            drawNoduleLine($(this).find('.block-nodule'));
+                        }
+                    });
+            return a;
+        }
+    },
 };
 
 var blockContainer;
 var lineOverlay;
 var noduleRadius;
 
-var curText = null;
+var curText   = null;
 var hintPopup = false;
 
-$(document).on('mousedown',function(){
-	console.log("hint popup: " + hintPopup);
-	if (hintPopup) {
-		if (curText && curText.length > 0) {
-			$("#bubbler").text(curText.shift());
-		}
-		else {
-			$("#bubbler").hide();
-			hintPopup = false;
-		}
-	}
+$(document).on('mousedown', function() {
+    console.log('hint popup: ' + hintPopup);
+    if (hintPopup) {
+        if (curText && curText.length > 0) {
+            rightDialog.text(curText.shift());
+        } else {
+            rightDialog.hide();
+            hintPopup = false;
+        }
+    }
 });
 
 function makeBlock(name, state) {
-	var a = blocks[name].create(state);
-	a.css("position", "absolute");
-	a.addClass("holder");
-	a.attr("data-blocktype", name);
-	a.find(".block-nodule").draggable({
-		drag: function (e, ui) {
-			var b = $(this);
-			drawNoduleLine(b);
-		}
-	});
-	a.click(function(){
-		curText = state.hint;
-		$("#bubbler").text(curText.shift());
-		$("#bubbler").show();
-		hintPopup = true;
+    var a = blocks[name].create(state);
+    a.css('position', 'absolute');
+    a.addClass('holder');
+    a.attr('data-blocktype', name);
+    a.find('.block-nodule').draggable({
+        drag: function(e, ui) {
+            var b = $(this);
+            drawNoduleLine(b);
+        }
+    });
+    a.click(function() {
+        curText = state.hint;
+        rightDialog.text(curText.shift());
+        rightDialog.show();
+        hintPopup = true;
 
-		// alert(state.hint);
-	});
-	if (state && state.output) {
-		a.find(".block").text("If: " + state.output);
-		a.attr("data-output", state.output);
-	} else if (state && state.value) a.find(".block").text(state.value);
-	if (state && state.hint) a.attr("data-hint", JSON.stringify(state.hint));
-	if (state && state.input) a.attr("data-input", state.input);
-	a.uniqueId();
-	return a;
+        // alert(state.hint);
+    });
+    if (state && state.output) {
+        a.find('.block').text('If: ' + state.output);
+        a.attr('data-output', state.output);
+    } else if (state && state.value)
+        a.find('.block').text(state.value);
+    if (state && state.hint)
+        a.attr('data-hint', JSON.stringify(state.hint));
+    if (state && state.input)
+        a.attr('data-input', state.input);
+    a.uniqueId();
+    return a;
 }
 
 function drawNoduleLine(b) {
-	if (!noduleRadius) {
-		var nodule = $(".block-nodule");
-		if (nodule.length) {
-			noduleRadius = $(nodule[0]).outerWidth()/2;
-		}		
-	}
-	console.log(noduleRadius);
+    if (!noduleRadius) {
+        var nodule = $('.block-nodule');
+        if (nodule.length) {
+            noduleRadius = $(nodule[0]).outerWidth() / 2;
+        }
+    }
+    console.log(noduleRadius);
 
-	var l = createLine(b.parent().offset().left, 
-		b.parent().offset().top + (0.5*b.parent().height()) - 4, 
-		b.offset().left + noduleRadius - 6, 
-		b.offset().top + noduleRadius - 4,
-		b.parent().attr("id")+"-line");
+    var l = createLine(
+        b.parent().offset().left, b.parent().offset().top + (0.5 * b.parent().height()) - 4,
+        b.offset().left + noduleRadius - 6, b.offset().top + noduleRadius - 4,
+        b.parent().attr('id') + '-line');
 }
 
 function checkSolution() {
-	var terminating = $(".outvar").parent();
-	var a = getVal($(".block-nodule"), terminating, []);
-	console.log(a);
-	var passed = a == terminating.find(".block").text();
-	setPassed(passed);
+    var terminating = $('.outvar').parent();
+    var a           = getVal($('.block-nodule'), terminating, []);
+    console.log(a);
+    var passed = a == terminating.find('.block').text();
+    setPassed(passed);
 }
 
 function setPassed(passed) {
-
-	if (passed) {$("#bubble").show(); $("#bubble").html("<p>You did it!</p><button class='block small' onClick='passedLevel()'>Continue to next level</button>" );} //$(".passed").show();
-	else $("#bubble").hide();
+    if (passed) {
+        leftDialog.show(100);
+        leftDialog.html(
+            '<p>You did it!</p><button class=\'block small\' onClick=\'passedLevel()\'>Continue to next level</button>');
+    }
 }
 
 function getVal(nodules, startNode, already) {
-	if (already.indexOf(startNode) != -1) {
-		return "FAIL-loop";
-	}
-	already.push(startNode);
-	var nodeType = startNode.attr("data-blocktype");
-	if (nodeType == "var" || nodeType == "subroutine") {
-		return startNode.find(".block").text();
-	}
-	var intersects = [];
-	// find all nodules on this node
-	for (var i = 0; i < nodules.length; i++) {
-		if ($(nodules[i]).hitTestObject(startNode)) {
-			// find the nodule's parent
-			var parent = $(nodules[i]).parent();
-			if (parent.attr("id") == startNode.attr("id")) continue;
-			intersects.push(parent);
-		}
-	}
-	//console.log(intersects);
-	if (nodeType == "combine") {
-		if (intersects.length != 2) return "FAIL-combine-length";
-		var first = getVal(nodules, intersects[0], already);
-		var second = getVal(nodules, intersects[1], already);
-		var combo = combinations[first + ":" + second];
-		if (combo) return combo;
-		combo = combinations[second + ":" + first];
-		if (combo) return combo;
-		return "FAIL-combine";
-	}
-	if (nodeType == "outvar") {
-		if (intersects.length != 1) return "FAIL-outvar";
-		return getVal(nodules, intersects[0], already);
-	}
-	if (nodeType == "if") {
-		if (intersects.length != 1) return "FAIL-if";
-		var first = getVal(nodules, intersects[0], already);
-		if (first != startNode.attr("data-input")) return "FAIL-if-val";
-		return startNode.attr("data-output");
-	}
-	if (nodeType == "for") {
-		if (intersects.length != 2) return "FAIL-for";
-		var first = getVal(nodules, intersects[0], already);
-		var second = getVal(nodules, intersects[1], already);
-		if(first == "water" && second == "carrot_seeds") return "carrots";
-		if(first == "carrot_seeds" && second == "water") return "carrots";
-		return "FAIL";
-	}
-	return "FAIL-unknown";
+    if (already.indexOf(startNode) != -1) {
+        return 'FAIL-loop';
+    }
+    already.push(startNode);
+    var nodeType = startNode.attr('data-blocktype');
+    if (nodeType == 'var' || nodeType == 'subroutine') {
+        return startNode.find('.block').text();
+    }
+    var intersects = [];
+    // find all nodules on this node
+    for (var i = 0; i < nodules.length; i++) {
+        if ($(nodules[i]).hitTestObject(startNode)) {
+            // find the nodule's parent
+            var parent = $(nodules[i]).parent();
+            if (parent.attr('id') == startNode.attr('id'))
+                continue;
+            intersects.push(parent);
+        }
+    }
+    // console.log(intersects);
+    if (nodeType == 'combine') {
+        if (intersects.length != 2)
+            return 'FAIL-combine-length';
+        var first  = getVal(nodules, intersects[0], already);
+        var second = getVal(nodules, intersects[1], already);
+        var combo  = combinations[first + ':' + second];
+        if (combo)
+            return combo;
+        combo = combinations[second + ':' + first];
+        if (combo)
+            return combo;
+        return 'FAIL-combine';
+    }
+    if (nodeType == 'outvar') {
+        if (intersects.length != 1)
+            return 'FAIL-outvar';
+        return getVal(nodules, intersects[0], already);
+    }
+    if (nodeType == 'if') {
+        if (intersects.length != 1)
+            return 'FAIL-if';
+        var first = getVal(nodules, intersects[0], already);
+        if (first != startNode.attr('data-input'))
+            return 'FAIL-if-val';
+        return startNode.attr('data-output');
+    }
+    if (nodeType == 'for') {
+        if (intersects.length != 2)
+            return 'FAIL-for';
+        var first  = getVal(nodules, intersects[0], already);
+        var second = getVal(nodules, intersects[1], already);
+        if (first == 'water' && second == 'carrot_seeds')
+            return 'carrots';
+        if (first == 'carrot_seeds' && second == 'water')
+            return 'carrots';
+        return 'FAIL';
+    }
+    return 'FAIL-unknown';
 }
 
 function createBlocks() {
-	blockContainer.empty();
-	for (var i = 0; i < curLevel.init.length; i++) {
-		var a = makeBlock(curLevel.init[i].block.type, curLevel.init[i].block);
-		blockContainer.append(a);
-		a.css("left", curLevel.init[i].position.x*30 + "px").css("top", curLevel.init[i].position.y*30 + "px");
-	}
+    blockContainer.empty();
+    for (var i = 0; i < curLevel.init.length; i++) {
+        var a = makeBlock(curLevel.init[i].block.type, curLevel.init[i].block);
+        blockContainer.append(a);
+        a.css('left', curLevel.init[i].position.x * 30 + 'px')
+            .css('top', curLevel.init[i].position.y * 30 + 'px');
+    }
 
-	for (var i = 0; i < curLevel.draggables.length; i++) {
-		var dragged = makeBlock(curLevel.draggables[i].type, curLevel.draggables[i]);
-		dragged.css("right", "0px").css("top", i*50+"px");
-		blockContainer.append(dragged);
-	}
+    for (var i = 0; i < curLevel.draggables.length; i++) {
+        var dragged = makeBlock(curLevel.draggables[i].type, curLevel.draggables[i]);
+        dragged.css('right', '0px').css('top', i * 50 + 'px');
+        blockContainer.append(dragged);
+    }
 }
 
-function createLine(x1,y1, x2,y2, existingLineID) {
-	// draw manhattan lines
+function createLine(x1, y1, x2, y2, existingLineID) {
+    // draw manhattan lines
 
-  	x1 = Math.round(x1);
-  	x2 = Math.round(x2);
-  	y1 = Math.round(y1);
-  	y2 = Math.round(y2);
+    x1 = Math.round(x1);
+    x2 = Math.round(x2);
+    y1 = Math.round(y1);
+    y2 = Math.round(y2);
 
 
-   var line = $("#"+existingLineID);
-   if (line.length) {
-   		line.attr("points",[x1,",",y1, " ", x2,",",y1, " ", x2,",",y2].join(""));
-   }
-   else {
-	    line = $(document.createElementNS("http://www.w3.org/2000/svg","polyline"))
-	        .appendTo(lineOverlay)
-	        .attr("class", "line")
-	        .attr("id", existingLineID)
-	        .attr("stroke-dasharray", "10,10")
-	        .attr("points",[x1,",",y1, " ", x2,",",y1, " ", x2,",",y2].join(""));
-   }
+    var line = $('#' + existingLineID);
+    if (line.length) {
+        line.attr('points', [x1, ',', y1, ' ', x2, ',', y1, ' ', x2, ',', y2].join(''));
+    } else {
+        line = $(document.createElementNS('http://www.w3.org/2000/svg', 'polyline'))
+                   .appendTo(lineOverlay)
+                   .attr('class', 'line')
+                   .attr('id', existingLineID)
+                   .attr('stroke-dasharray', '10,10')
+                   .attr('points', [x1, ',', y1, ' ', x2, ',', y1, ' ', x2, ',', y2].join(''));
+    }
 
     return line;
 }
 
 $(document).ready(function(e) {
-	blockContainer = $(".block-container");
-	lineOverlay = $("#lineoverlay");
+    blockContainer = $('.block-container');
+    lineOverlay    = $('#lineoverlay');
 });
 
-$(document).on("mousemove", function(e) {
-	if (e.which == 0) return;
-	checkSolution();
+$(document).on('mousemove', function(e) {
+    if (e.which == 0)
+        return;
+    checkSolution();
 });
 
 /*
 $(document).ready(function(e) {
-	//$("body").append(makeBlock("for", null));
-	$("body").append(makeBlock("var", {value: 42}));
-	//$("body").append(makeBlock("for", null));
-	$("body").append(makeBlock("combine", null));
-	$("body").append(makeBlock("subroutine", null));
+    //$("body").append(makeBlock("for", null));
+    $("body").append(makeBlock("var", {value: 42}));
+    //$("body").append(makeBlock("for", null));
+    $("body").append(makeBlock("combine", null));
+    $("body").append(makeBlock("subroutine", null));
 });
 */
--- nofun.js
+++ nofun.js
@@ -1,127 +1,140 @@
-"use strict";
-const DIALOGUE = 1;
-const INGAME = 0;
-var state = DIALOGUE;
-var currentScenes = [];
+'use strict';
+const DIALOGUE        = 1;
+const INGAME          = 0;
+var state             = DIALOGUE;
+var currentScenes     = [];
 var currentSceneIndex = 0;
-var currentTextIndex = 0;
-var curLevel = null;
-var endSceneCallback = null;
+var currentTextIndex  = 0;
+var curLevel          = null;
+var endSceneCallback  = null;
+var leftDialog        = null;
+var rightDialog       = null;
 function advanceDialogue() {
-	if (advanceText()) return;
-	advanceScene();
+    if (advanceText())
+        return;
+    advanceScene();
 }
 
 var combinations = {
-	"frames:lenses":"sunglasses",
-	"rocks:snow":"malformed coats",
-	"nice rocks:snow":"snowcoats",
-	"sun:snow":"water",
-	"water:carrot_seeds":"carrots",
-	"snow:carrot_seeds":"cold carrot seeds",
-	"snowman_core:snowman_body":"snowman",
-	"snow:snowcoats":"snowman body without nose",
-	"snowman body without nose:carrots":"snowman_body"
+    'frames:lenses': 'sunglasses',
+    'rocks:snow': 'malformed coats',
+    'nice rocks:snow': 'snowcoats',
+    'sun:snow': 'water',
+    'water:carrot_seeds': 'carrots',
+    'snow:carrot_seeds': 'cold carrot seeds',
+    'snowman_core:snowman_body': 'snowman',
+    'snow:snowcoats': 'snowman body without nose',
+    'snowman body without nose:carrots': 'snowman_body'
 }
 
-var imgType = ".png";
-var imgFolder = "img/";
+var imgType   = '.png';
+var imgFolder = 'img/';
 
 function advanceScene() {
-	var newsceneind = ++currentSceneIndex;
-	if (newsceneind >= currentScenes.length) {
-		endScene();
-		return;
-	}
-	var newScene = currentScenes[newsceneind];
-	$(".scene-speaker").attr("src", imgFolder + newScene.speaker + ".png").attr("alt", imgFolder + newScene.speaker + ".png");
-	if (newScene.position === "left") {
-		$(".scene-speaker").addClass("left");
-		$(".scene-speaker").removeClass("right");
-	}
-	else {
-		$(".scene-speaker").removeClass("left");
-		$(".scene-speaker").addClass("right");		
-	}
-	currentTextIndex = -1;
-	advanceText();
+    var newsceneind = ++currentSceneIndex;
+    if (newsceneind >= currentScenes.length) {
+        endScene();
+        return;
+    }
+    var newScene = currentScenes[newsceneind];
+    $('.scene-speaker')
+        .attr('src', imgFolder + newScene.speaker + '.png')
+        .attr('alt', imgFolder + newScene.speaker + '.png');
+    if (newScene.position === 'left') {
+        $('.scene-speaker').addClass('left');
+        $('.scene-speaker').removeClass('right');
+    } else {
+        $('.scene-speaker').removeClass('left');
+        $('.scene-speaker').addClass('right');
+    }
+    currentTextIndex = -1;
+    advanceText();
 }
 
+
 function advanceText() {
-	++currentTextIndex;
-	if (currentTextIndex >= currentScenes[currentSceneIndex].text.length) {
-		$("#bubbler").hide();
-		$("#bubble").hide();
-		return false;
-	}
-	if (currentScenes[currentSceneIndex].position === "left") {
-		$("#bubble").text(currentScenes[currentSceneIndex].text[currentTextIndex]);
-		$("#bubble").show();
-		$("#bubbler").hide();
-	}
-	else {
-		$("#bubbler").text(currentScenes[currentSceneIndex].text[currentTextIndex]);
-		$("#bubbler").show();
-		$("#bubble").hide();
-	}
-	return true;
+    ++currentTextIndex;
+    if (currentTextIndex >= currentScenes[currentSceneIndex].text.length) {
+        rightDialog.hide();
+        leftDialog.hide();
+        return false;
+    }
+    if (currentScenes[currentSceneIndex].position === 'left') {
+        leftDialog.text(currentScenes[currentSceneIndex].text[currentTextIndex]);
+        console.log('showing left');
+        leftDialog.show(100);
+        rightDialog.hide();
+    } else {
+        rightDialog.text(currentScenes[currentSceneIndex].text[currentTextIndex]);
+        console.log('showing right');
+        rightDialog.show(100);
+        leftDialog.hide();
+    }
+    return true;
 }
 
 function endScene() {
-	state = INGAME;
-	$(".scene-stage").hide();
-	if (endSceneCallback) endSceneCallback();
+    state = INGAME;
+    $('.scene-stage').hide();
+    if (endSceneCallback) {
+        endSceneCallback();
+    }
 }
 
 function startScene(sceneName) {
-	$(".scene-stage").show();
-	$(".block-container").hide();
-	state = DIALOGUE;
-	currentScenes = curLevel[sceneName];
-	currentSceneIndex = -1;
-	currentTextIndex = -1;
-	advanceScene();
+    $('.block-container').hide();
+    $('.scene-stage').show(100);
+    state             = DIALOGUE;
+    currentScenes     = curLevel[sceneName];
+    currentSceneIndex = -1;
+    currentTextIndex  = -1;
+    advanceScene();
 }
 
 function mouseDownHandler(e) {
-	if (state != DIALOGUE) return;
-	advanceDialogue();
+    if (state != DIALOGUE) {
+        return;
+    }
+    advanceDialogue();
 }
 function loadLevel(levelIndex) {
-	$.get("levels/level_" + levelIndex + ".json", null, initLevel, "text");
+    $.get('levels/level_' + levelIndex + '.json', null, initLevel, 'text');
 }
 
 var curLevelIndex = 1;
 
 function initUi() {
-	setPassed(false);
-	$(document).on("mousedown", mouseDownHandler);
-	loadLevel(curLevelIndex);
+    leftDialog  = $('#bubble');
+    rightDialog = $('#bubbler');
+    setPassed(false);
+    $(document).on('mousedown', mouseDownHandler);
+    loadLevel(curLevelIndex);
 }
 
 function initLevel(json) {
-	console.log(initLevel);
-	curLevel = JSON.parse(json);
-	endSceneCallback = function(e) {
-		$(".block-container").show();
-	}
-	startScene("scenesbefore");
-	createBlocks();
-	setPassed(false);
+    console.log(initLevel);
+    curLevel         = JSON.parse(json);
+    endSceneCallback = function(e) {
+        $('.block-container').show();
+    };
+    startScene('scenesbefore');
+    createBlocks();
+    setPassed(false);
 }
 
 function passedLevel() {
-	$(".line").remove();
-	endSceneCallback = function(e) {
-		curLevelIndex++;
-		if (curLevelIndex > 4) {
-			alert("You've completed the challenge!");
-			return;
-		}
-		loadLevel(curLevelIndex);
-	};
-	startScene("scenesafter");
-	setPassed(false);
+    $('.line').remove();
+    endSceneCallback = function(e) {
+        curLevelIndex++;
+        console.log('Now on level ' + curLevelIndex);
+        if (curLevelIndex > 4) {
+            alert('You"ve completed the challenge!');
+            return;
+        }
+        loadLevel(curLevelIndex);
+    };
+    startScene('scenesafter');
+    setPassed(false);
 }
 
 $(document).ready(initUi);
